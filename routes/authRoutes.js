f4d3b9daf7f27f561cb65ac8d124817f6de14e96
bhautik2005 <gondaliyabhautik419@gmail.com>
2026-02-26 08:33:56 +0530

    Add redirects for legacy /singup paths

--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -7,8 +7,13 @@ const authControllers = require('../Controllers/authControllers');
 authRoutes.get('/login', authControllers.getLogin );
 authRoutes.post('/login', authControllers.postLogin);
 authRoutes.post('/logout', authControllers.postLogout);
+// new correct signup routes
 authRoutes.get('/signup', authControllers.getSignup);
 authRoutes.post('/signup', authControllers.postSignup);
+
+// legacy spelling redirection (clients may still request /singup)
+authRoutes.get('/singup', (req, res) => res.redirect('/signup'));
+authRoutes.post('/singup', (req, res) => res.redirect(307, '/signup'));
 authRoutes.get('/forgot-password',authControllers.getForgotPassword);
 authRoutes.post('/forgot-password',authControllers.postForgotPassword);
 authRoutes.get('/reset-password/:token',authControllers.getResetPassword);
